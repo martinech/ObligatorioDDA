907f99fc61122d1f17289cdf3d35a2c1127e44f0
Martin Echenique <martin.echenique@hotmail.com>
2025-06-09 10:32:58 -0300

    Created classes

---
+++ src/dominio/Insumo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dominio;
+
+/**
+ *
+ * @author marti
+ */
+class Insumo {
+    
+    private String nombre;
+    
+    private int stockMin;
+    
+    private int stockAct;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getStockMin() {
+        return stockMin;
+    }
+
+    public int getStockAct() {
+        return stockAct;
+    }
+    
+}
